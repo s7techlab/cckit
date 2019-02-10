15ee131958385acfc0fb68e12bdef3ad3da2f282
vitiko <vitiko@mail.ru>
2019-02-10 22:14:13 +0300

    WIP: protobuf state

--- examples/cpaper/testdata/testdata.go
+++ examples/cpaper/testdata/testdata.go
@@ -3,32 +3,30 @@ package testdata
 import (
 	"time"
 
+	"github.com/s7techlab/cckit/testing"
+
 	"github.com/golang/protobuf/ptypes"
 	"github.com/s7techlab/cckit/examples/cpaper/schema"
 )
 
 var (
-	maturityDate1, _ = ptypes.TimestampProto(time.Now().AddDate(0, 1, 0))
-	maturityDate2, _ = ptypes.TimestampProto(time.Now().AddDate(0, 2, 0))
-	maturityDate3, _ = ptypes.TimestampProto(time.Now().AddDate(0, 3, 0))
-
 	// CPapers commercial paper fixtures
 	CPapers = []schema.IssueCommercialPaper{{
 		Issuer:       `some-issuer-1`,
 		PaperNumber:  `00000001`,
 		IssueDate:    ptypes.TimestampNow(),
-		MaturityDate: maturityDate1,
+		MaturityDate: testing.MustProtoTimestamp(time.Now().AddDate(0, 1, 0)),
 		FaceValue:    11111,
 	}, {
 		Issuer:       `some-issuer-2`,
 		PaperNumber:  `00000002`,
 		IssueDate:    ptypes.TimestampNow(),
-		MaturityDate: maturityDate2,
+		MaturityDate: testing.MustProtoTimestamp(time.Now().AddDate(0, 2, 0)),
 		FaceValue:    22222,
 	}, {
 		Issuer:       `some-issuer-3`,
 		PaperNumber:  `00000003`,
 		IssueDate:    ptypes.TimestampNow(),
-		MaturityDate: maturityDate3,
+		MaturityDate: testing.MustProtoTimestamp(time.Now().AddDate(0, 3, 0)),
 	}}
 )
--- testing/must.go
+++ testing/must.go
@@ -1,7 +1,12 @@
 package testing
 
 import (
+	"time"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
 )
 
 func MustProtoMarshal(pb proto.Message) []byte {
@@ -10,4 +15,12 @@ func MustProtoMarshal(pb proto.Message) []byte {
 		panic(err)
 	}
 	return bb
+}
+
+func MustProtoTimestamp(t time.Time) *timestamp.Timestamp {
+	ts, err := ptypes.TimestampProto(t)
+	if err != nil {
+		panic(err)
+	}
+	return ts
 }
