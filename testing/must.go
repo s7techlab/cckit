9b0a89ee19b7f8cc5433a403bd1a9f7267e374bc
vitiko <vitiko@mail.ru>
2019-03-15 15:30:29 +0300

    proto helper

--- testing/must.go
+++ testing/must.go
@@ -9,6 +9,7 @@ import (
 	"github.com/golang/protobuf/ptypes"
 )
 
+// MustProtoMarshal marshals proto.Message, panics if error
 func MustProtoMarshal(pb proto.Message) []byte {
 	bb, err := proto.Marshal(pb)
 	if err != nil {
@@ -17,6 +18,16 @@ func MustProtoMarshal(pb proto.Message) []byte {
 	return bb
 }
 
+// MustProtoUnmarshal unmarshals proto.Message, panics if error
+func MustProtoUnmarshal(bb []byte, pm proto.Message) proto.Message {
+	p := proto.Clone(pm)
+	if err := proto.Unmarshal(bb, p); err != nil {
+		panic(err)
+	}
+	return p
+}
+
+// MustProtoTimestamp, creates proto.Timestamp, panics if error
 func MustProtoTimestamp(t time.Time) *timestamp.Timestamp {
 	ts, err := ptypes.TimestampProto(t)
 	if err != nil {
