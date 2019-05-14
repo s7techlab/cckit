6428dd30c865394c883466c73fc0497b0c606418
Antonio Martinović <martinovic.antonio@gmail.com>
2019-05-14 23:18:55 +0200

    Fix lint error about SNAKE_CASE, correct BuyerName

--- examples/cpaper/chaincode_test.go
+++ examples/cpaper/chaincode_test.go
@@ -12,13 +12,13 @@ import (
 )
 
 const (
-	ISSUER_NAME = "SomeIssuer"
-	BUYER_NAME  = "SomeIssuer"
+	IssuerName = "SomeIssuer"
+	BuyerName  = "SomeBuyer"
 )
 
 func TestCommercialPaper(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Cars Suite")
+	RunSpecs(t, "Commercial Paper Suite")
 }
 
 var _ = Describe(`CommercialPaper`, func() {
@@ -33,7 +33,7 @@ var _ = Describe(`CommercialPaper`, func() {
 		It("Allow issuer to issue new commercial paper", func() {
 			//invoke chaincode method from authority actor
 			expectcc.ResponseOk(paperChaincode.Invoke(`issue`, &IssueCommercialPaper{
-				Issuer:       ISSUER_NAME,
+				Issuer:       IssuerName,
 				PaperNumber:  "0001",
 				IssueDate:    time.Now(),
 				MaturityDate: time.Now().Add(time.Hour * 24 * 30 * 6), // Six months later
@@ -42,11 +42,11 @@ var _ = Describe(`CommercialPaper`, func() {
 		})
 
 		It("Allow issuer to get commercial paper", func() {
-			queryResponse := paperChaincode.Query("get", ISSUER_NAME, "0001")
+			queryResponse := paperChaincode.Query("get", IssuerName, "0001")
 			paper := expectcc.PayloadIs(queryResponse, &CommercialPaper{}).(CommercialPaper)
 
-			Expect(paper.Issuer).To(Equal(ISSUER_NAME))
-			Expect(paper.Owner).To(Equal(ISSUER_NAME))
+			Expect(paper.Issuer).To(Equal(IssuerName))
+			Expect(paper.Owner).To(Equal(IssuerName))
 			Expect(paper.State).To(Equal(CommercialPaperIssued))
 			Expect(paper.PaperNumber).To(Equal("0001"))
 			Expect(paper.FaceValue).To(BeNumerically("==", 100000))
@@ -57,8 +57,8 @@ var _ = Describe(`CommercialPaper`, func() {
 			papers := expectcc.PayloadIs(queryResponse, &[]CommercialPaper{}).([]CommercialPaper)
 
 			Expect(len(papers)).To(BeNumerically("==", 1))
-			Expect(papers[0].Issuer).To(Equal(ISSUER_NAME))
-			Expect(papers[0].Owner).To(Equal(ISSUER_NAME))
+			Expect(papers[0].Issuer).To(Equal(IssuerName))
+			Expect(papers[0].Owner).To(Equal(IssuerName))
 			Expect(papers[0].State).To(Equal(CommercialPaperIssued))
 			Expect(papers[0].PaperNumber).To(Equal("0001"))
 			Expect(papers[0].FaceValue).To(BeNumerically("==", 100000))
@@ -67,32 +67,32 @@ var _ = Describe(`CommercialPaper`, func() {
 		It("Allow buyer to buy commercial paper", func() {
 			//invoke chaincode method from authority actor
 			expectcc.ResponseOk(paperChaincode.Invoke(`buy`, &BuyCommercialPaper{
-				Issuer:       ISSUER_NAME,
+				Issuer:       IssuerName,
 				PaperNumber:  "0001",
-				CurrentOwner: ISSUER_NAME,
-				NewOwner:     BUYER_NAME,
+				CurrentOwner: IssuerName,
+				NewOwner:     BuyerName,
 				Price:        95000,
 				PurchaseDate: time.Now(),
 			}))
 
-			queryResponse := paperChaincode.Query("get", ISSUER_NAME, "0001")
+			queryResponse := paperChaincode.Query("get", IssuerName, "0001")
 			paper := expectcc.PayloadIs(queryResponse, &CommercialPaper{}).(CommercialPaper)
-			Expect(paper.Owner).To(Equal(BUYER_NAME))
+			Expect(paper.Owner).To(Equal(BuyerName))
 			Expect(paper.State).To(Equal(CommercialPaperTrading))
 		})
 
 		It("Allow buyer to redeem commercial paper", func() {
 			//invoke chaincode method from authority actor
 			expectcc.ResponseOk(paperChaincode.Invoke(`redeem`, &RedeemCommercialPaper{
-				Issuer:         ISSUER_NAME,
+				Issuer:         IssuerName,
 				PaperNumber:    "0001",
-				RedeemingOwner: BUYER_NAME,
+				RedeemingOwner: BuyerName,
 				RedeemDate:     time.Now(),
 			}))
 
-			queryResponse := paperChaincode.Query("get", ISSUER_NAME, "0001")
+			queryResponse := paperChaincode.Query("get", IssuerName, "0001")
 			paper := expectcc.PayloadIs(queryResponse, &CommercialPaper{}).(CommercialPaper)
-			Expect(paper.Owner).To(Equal(ISSUER_NAME))
+			Expect(paper.Owner).To(Equal(IssuerName))
 			Expect(paper.State).To(Equal(CommercialPaperRedeemed))
 		})
 	})
