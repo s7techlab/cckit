f7628c6a4c0b48bd2c7d7a8549b8b88fd5257a01
Antonio Martinović <martinovic.antonio@gmail.com>
2019-05-14 22:50:30 +0200

    Fix docs function names, add single identity load method

--- testing/identity.go
+++ testing/identity.go
@@ -39,7 +39,7 @@ func IdentityFromPem(mspID string, certPEM []byte) (*Identity, error) {
 	return NewIdentity(mspID, certIdentity.Cert), nil
 }
 
-// ActorsFromPem returns CertIdentity (MSP ID and X.509 cert) converted PEM content
+// IdentitiesFromPem returns CertIdentity (MSP ID and X.509 cert) converted PEM content
 func IdentitiesFromPem(mspID string, certPEMs map[string][]byte) (ids Identities, err error) {
 	identities := make(Identities)
 	for role, certPEM := range certPEMs {
@@ -50,7 +50,7 @@ func IdentitiesFromPem(mspID string, certPEMs map[string][]byte) (ids Identities
 	return identities, nil
 }
 
-// ActorsFromPemFile returns map of CertIdentity, loaded from PEM files
+// IdentitiesFromFiles returns map of CertIdentity, loaded from PEM files
 func IdentitiesFromFiles(mspID string, files map[string]string, getContent identity.GetContent) (Identities, error) {
 	contents := make(map[string][]byte)
 	for key, filename := range files {
@@ -61,6 +61,16 @@ func IdentitiesFromFiles(mspID string, files map[string]string, getContent ident
 		contents[key] = content
 	}
 	return IdentitiesFromPem(mspID, contents)
+}
+
+// IdentityFromFile returns Identity struct containing mspId and certificate
+func IdentityFromFile(mspID string, file string, getContent identity.GetContent) (*Identity, error) {
+	content, err := getContent(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return IdentityFromPem(mspID, content)
 }
 
 //  MustIdentitiesFromFiles
