c83bb612e85fbe9bf401675ca580b232313b397b
Antonio Martinović <martinovic.antonio@gmail.com>
2019-05-14 21:50:24 +0200

    Simplify cpaper example and add tests
    * cpaper example follows the basic logic from original example
    * remove proto schema data from basic example
    * cpaper tests provide usage and further explanation

--- examples/cpaper/chaincode.go
+++ examples/cpaper/chaincode.go
@@ -2,149 +2,200 @@ package cpaper
 
 import (
 	"fmt"
+	"github.com/s7techlab/cckit/router/param"
+	"time"
 
 	"github.com/pkg/errors"
-	"github.com/s7techlab/cckit/examples/cpaper/schema"
-	"github.com/s7techlab/cckit/extensions/debug"
-	"github.com/s7techlab/cckit/extensions/owner"
 	"github.com/s7techlab/cckit/router"
-	"github.com/s7techlab/cckit/router/param/defparam"
-	m "github.com/s7techlab/cckit/state/mapping"
 )
 
-var (
-	// State mappings
-	StateMappings = m.StateMappings{}.
-		//  Create mapping for Commercial Paper entity
-		Add(&schema.CommercialPaper{},
-			m.PKeySchema(&schema.CommercialPaperId{}), // Key namespace will be <`CommercialPaper`, Issuer, PaperNumber>
-			m.List(&schema.CommercialPaperList{}),     // Structure of result for List method
-		)
+type TaskState string
 
-	// EventMappings
-	EventMappings = m.EventMappings{}.
-		// Event name will be `IssueCommercialPaper`, payload - same as issue payload
-		Add(&schema.IssueCommercialPaper{}).
-		// Event name will be `BuyCommercialPaper`
-		Add(&schema.BuyCommercialPaper{}).
-		// Event name will be `RedeemCommercialPaper`
-		Add(&schema.RedeemCommercialPaper{})
+const (
+	CommercialPaperTypeName = "CommercialPaper"
+
+	CommercialPaperIssued   TaskState = "issued"
+	CommercialPaperTrading  TaskState = "trading"
+	CommercialPaperRedeemed TaskState = "redeemed"
 )
+
+type CommercialPaper struct {
+	Issuer       string    `json:"issuer,omitempty"`
+	PaperNumber  string    `json:"paper_number,omitempty"`
+	Owner        string    `json:"owner,omitempty"`
+	IssueDate    time.Time `json:"issue_date,omitempty"`
+	MaturityDate time.Time `json:"maturity_date,omitempty"`
+	FaceValue    int32     `json:"face_value,omitempty"`
+	State        TaskState `json:"state,omitempty"`
+}
+
+// Comercial paper has a composite key <`CommercialPaper`, Issuer, PaperNumber>
+func (c CommercialPaper) Key() ([]string, error) {
+	return []string{CommercialPaperTypeName, c.Issuer, c.PaperNumber}, nil
+}
+
+type IssueCommercialPaper struct {
+	Issuer       string    `json:"issuer,omitempty"`
+	PaperNumber  string    `json:"paper_number,omitempty"`
+	IssueDate    time.Time `json:"issue_date,omitempty"`
+	MaturityDate time.Time `json:"maturity_date,omitempty"`
+	FaceValue    int32     `json:"face_value,omitempty"`
+}
+
+type BuyCommercialPaper struct {
+	Issuer       string    `json:"issuer,omitempty"`
+	PaperNumber  string    `json:"paper_number,omitempty"`
+	CurrentOwner string    `json:"current_owner,omitempty"`
+	NewOwner     string    `json:"new_owner,omitempty"`
+	Price        int32     `json:"price,omitempty"`
+	PurchaseDate time.Time `json:"purchase_date,omitempty"`
+}
+
+type RedeemCommercialPaper struct {
+	Issuer         string    `json:"issuer,omitempty"`
+	PaperNumber    string    `json:"paper_number,omitempty"`
+	RedeemingOwner string    `json:"redeeming_owner,omitempty"`
+	RedeemDate     time.Time `json:"redeem_date,omitempty"`
+}
 
 func NewCC() *router.Chaincode {
-
 	r := router.New(`commercial_paper`)
 
-	// Mappings for chaincode state
-	r.Use(m.MapStates(StateMappings))
-
-	// Mappings for chaincode events
-	r.Use(m.MapEvents(EventMappings))
-
-	// Store on the ledger the information about chaincode instantiator
-	r.Init(owner.InvokeSetFromCreator)
-
-	// Method for debug chaincode state
-	debug.AddHandlers(r, `debug`, owner.Only)
+	r.Init(func(context router.Context) (i interface{}, e error) {
+		// No implementation required with this example
+		// It could be where data migration is performed, if necessary
+		return nil, nil
+	})
 
 	r.
 		// Read methods
-		Query(`list`, cpaperList).
-
+		Query(`list`, list).
 		// Get method has 2 params - commercial paper primary key components
-		Query(`get`, cpaperGet, defparam.Proto(&schema.CommercialPaperId{})).
+		Query(`get`, get, param.String("issuer"), param.String("paper_number")).
 
 		// Transaction methods
-		Invoke(`issue`, cpaperIssue, defparam.Proto(&schema.IssueCommercialPaper{})).
-		Invoke(`buy`, cpaperBuy, defparam.Proto(&schema.BuyCommercialPaper{})).
-		Invoke(`redeem`, cpaperRedeem, defparam.Proto(&schema.RedeemCommercialPaper{})).
-		Invoke(`delete`, cpaperDelete, defparam.Proto(&schema.CommercialPaperId{}))
+		Invoke(`issue`, issue, param.Struct("issueData", &IssueCommercialPaper{})).
+		Invoke(`buy`, buy, param.Struct("buyData", &BuyCommercialPaper{})).
+		Invoke(`redeem`, redeem, param.Struct("redeemData", &RedeemCommercialPaper{}))
 
 	return router.NewChaincode(r)
 }
 
-func cpaperList(c router.Context) (interface{}, error) {
-	// List method retrieves all entries from the ledger using GetStateByPartialCompositeKey method and passing it the
-	// namespace of our contract type, in this example that's `CommercialPaper`, then it unmarshals received bytes via
-	// proto.Ummarshal method and creates a []schema.CommercialPaperList as defined in the
-	// `StateMappings` variable at the top of the file
-	return c.State().List(&schema.CommercialPaper{})
-}
-
-func cpaperIssue(c router.Context) (interface{}, error) {
+func get(c router.Context) (interface{}, error) {
 	var (
-		issue  = c.Param().(*schema.IssueCommercialPaper) // Assert the chaincode parameter
-		cpaper = &schema.CommercialPaper{
-			Issuer:       issue.Issuer,
-			PaperNumber:  issue.PaperNumber,
-			Owner:        issue.Issuer,
-			IssueDate:    issue.IssueDate,
-			MaturityDate: issue.MaturityDate,
-			FaceValue:    issue.FaceValue,
-			State:        schema.CommercialPaper_ISSUED, // Initial state
-		}
-		err error
+		issuer      = c.ParamString("issuer")
+		paperNumber = c.ParamString("paper_number")
 	)
 
-	if err = c.Event().Set(issue); err != nil {
-		return nil, err
-	}
-
-	return cpaper, c.State().Insert(cpaper)
+	return c.State().Get(&CommercialPaper{
+		Issuer:      issuer,
+		PaperNumber: paperNumber,
+	})
 }
 
-func cpaperBuy(c router.Context) (interface{}, error) {
+func list(c router.Context) (interface{}, error) {
+	// List method retrieves all entries from the ledger using GetStateByPartialCompositeKey method and passing it the
+	// namespace of our contract type, in this example that's `CommercialPaper`, then it unmarshals received bytes via
+	// json.Ummarshal method and creates a JSON array of CommercialPaper entities
+	return c.State().List(CommercialPaperTypeName, &CommercialPaper{})
+}
 
+func issue(c router.Context) (interface{}, error) {
 	var (
-		cpaper *schema.CommercialPaper
+		issueData       = c.Param("issueData").(IssueCommercialPaper) // Assert the chaincode parameter
+		commercialPaper = &CommercialPaper{
+			Issuer:       issueData.Issuer,
+			PaperNumber:  issueData.PaperNumber,
+			Owner:        issueData.Issuer,
+			IssueDate:    issueData.IssueDate,
+			MaturityDate: issueData.MaturityDate,
+			FaceValue:    issueData.FaceValue,
+			State:        CommercialPaperIssued, // Initial state
+		}
+	)
 
+	return commercialPaper, c.State().Insert(commercialPaper)
+}
+
+func buy(c router.Context) (interface{}, error) {
+	var (
+		commercialPaper CommercialPaper
 		// Buy transaction payload
-		buy = c.Param().(*schema.BuyCommercialPaper)
+		buyData = c.Param("buyData").(BuyCommercialPaper)
 
 		// Get the current commercial paper state
-		cp, err = c.State().Get(
-			&schema.CommercialPaperId{Issuer: buy.Issuer, PaperNumber: buy.PaperNumber},
-			&schema.CommercialPaper{})
+		cp, err = c.State().Get(&CommercialPaper{
+			Issuer:      buyData.Issuer,
+			PaperNumber: buyData.PaperNumber,
+		}, &CommercialPaper{})
 	)
 
 	if err != nil {
-		return nil, errors.Wrap(err, `not found`)
+		return nil, errors.Wrap(err, "paper not found")
 	}
-	cpaper = cp.(*schema.CommercialPaper)
+
+	commercialPaper = cp.(CommercialPaper)
 
 	// Validate current owner
-	if cpaper.Owner != buy.CurrentOwner {
-		return nil, fmt.Errorf(`paper %s %s is not owned by %s`, cpaper.Issuer, cpaper.PaperNumber, buy.CurrentOwner)
+	if commercialPaper.Owner != buyData.CurrentOwner {
+		return nil, fmt.Errorf(
+			"paper %s %s is not owned by %s",
+			commercialPaper.Issuer, commercialPaper.PaperNumber, buyData.CurrentOwner)
 	}
 
-	// First buy moves state from ISSUED to TRADING
-	if cpaper.State == schema.CommercialPaper_ISSUED {
-		cpaper.State = schema.CommercialPaper_TRADING
+	// First buyData moves state from ISSUED to TRADING
+	if commercialPaper.State == CommercialPaperIssued {
+		commercialPaper.State = CommercialPaperTrading
 	}
 
 	// Check paper is not already REDEEMED
-	if cpaper.State == schema.CommercialPaper_TRADING {
-		cpaper.Owner = buy.NewOwner
+	if commercialPaper.State == CommercialPaperTrading {
+		commercialPaper.Owner = buyData.NewOwner
 	} else {
-		return nil, fmt.Errorf(`paper %s %s is not trading.current state = %s`, cpaper.Issuer, cpaper.PaperNumber, cpaper.State)
+		return nil, fmt.Errorf(
+			"paper %s %s is not trading.current state = %s",
+			commercialPaper.Issuer, commercialPaper.PaperNumber, commercialPaper.State)
 	}
 
-	if err = c.Event().Set(buy); err != nil {
-		return nil, err
+	return commercialPaper, c.State().Put(commercialPaper)
+}
+
+func redeem(c router.Context) (interface{}, error) {
+	var (
+		commercialPaper CommercialPaper
+
+		// Buy transaction payload
+		redeemData = c.Param("redeemData").(RedeemCommercialPaper)
+
+		// Get the current commercial paper state
+		cp, err = c.State().Get(&CommercialPaper{
+			Issuer:      redeemData.Issuer,
+			PaperNumber: redeemData.PaperNumber,
+		}, &CommercialPaper{})
+	)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "paper not found")
 	}
 
-	return cpaper, c.State().Put(cpaper)
-}
+	commercialPaper = cp.(CommercialPaper)
 
-func cpaperRedeem(c router.Context) (interface{}, error) {
-	// implement me
-	return nil, nil
-}
+	// Check paper is not REDEEMED
+	if commercialPaper.State == CommercialPaperRedeemed {
+		return nil, fmt.Errorf(
+			"paper %s %s is already redeemed",
+			commercialPaper.Issuer, commercialPaper.PaperNumber)
+	}
 
-func cpaperGet(c router.Context) (interface{}, error) {
-	return c.State().Get(c.Param().(*schema.CommercialPaperId))
-}
+	// Verify that the redeemer owns the commercial paper before redeeming it
+	if commercialPaper.Owner == redeemData.RedeemingOwner {
+		commercialPaper.Owner = redeemData.Issuer
+		commercialPaper.State = CommercialPaperRedeemed
+	} else {
+		return nil, fmt.Errorf(
+			"redeeming owner does not own paper %s %s",
+			commercialPaper.Issuer, commercialPaper.PaperNumber)
+	}
 
-func cpaperDelete(c router.Context) (interface{}, error) {
-	return nil, c.State().Delete(c.Param().(*schema.CommercialPaperId))
+	return commercialPaper, c.State().Put(commercialPaper)
 }
--- examples/cpaper/chaincode_test.go
+++ examples/cpaper/chaincode_test.go
@@ -1,1 +1,99 @@
-package cpaper_test
+package cpaper
+
+import (
+	"testing"
+	"time"
+
+	testcc "github.com/s7techlab/cckit/testing"
+	expectcc "github.com/s7techlab/cckit/testing/expect"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	ISSUER_NAME = "SomeIssuer"
+	BUYER_NAME  = "SomeIssuer"
+)
+
+func TestCommercialPaper(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Cars Suite")
+}
+
+var _ = Describe(`CommercialPaper`, func() {
+	paperChaincode := testcc.NewMockStub(`commercial_paper`, NewCC())
+
+	BeforeSuite(func() {
+		expectcc.ResponseOk(paperChaincode.Init())
+	})
+
+	Describe("Commercial Paper lifecycle", func() {
+
+		It("Allow issuer to issue new commercial paper", func() {
+			//invoke chaincode method from authority actor
+			expectcc.ResponseOk(paperChaincode.Invoke(`issue`, &IssueCommercialPaper{
+				Issuer:       ISSUER_NAME,
+				PaperNumber:  "0001",
+				IssueDate:    time.Now(),
+				MaturityDate: time.Now().Add(time.Hour * 24 * 30 * 6), // Six months later
+				FaceValue:    100000,
+			}))
+		})
+
+		It("Allow issuer to get commercial paper", func() {
+			queryResponse := paperChaincode.Query("get", ISSUER_NAME, "0001")
+			paper := expectcc.PayloadIs(queryResponse, &CommercialPaper{}).(CommercialPaper)
+
+			Expect(paper.Issuer).To(Equal(ISSUER_NAME))
+			Expect(paper.Owner).To(Equal(ISSUER_NAME))
+			Expect(paper.State).To(Equal(CommercialPaperIssued))
+			Expect(paper.PaperNumber).To(Equal("0001"))
+			Expect(paper.FaceValue).To(BeNumerically("==", 100000))
+		})
+
+		It("Allow issuer to get a list of commercial papers", func() {
+			queryResponse := paperChaincode.Query("list")
+			papers := expectcc.PayloadIs(queryResponse, &[]CommercialPaper{}).([]CommercialPaper)
+
+			Expect(len(papers)).To(BeNumerically("==", 1))
+			Expect(papers[0].Issuer).To(Equal(ISSUER_NAME))
+			Expect(papers[0].Owner).To(Equal(ISSUER_NAME))
+			Expect(papers[0].State).To(Equal(CommercialPaperIssued))
+			Expect(papers[0].PaperNumber).To(Equal("0001"))
+			Expect(papers[0].FaceValue).To(BeNumerically("==", 100000))
+		})
+
+		It("Allow buyer to buy commercial paper", func() {
+			//invoke chaincode method from authority actor
+			expectcc.ResponseOk(paperChaincode.Invoke(`buy`, &BuyCommercialPaper{
+				Issuer:       ISSUER_NAME,
+				PaperNumber:  "0001",
+				CurrentOwner: ISSUER_NAME,
+				NewOwner:     BUYER_NAME,
+				Price:        95000,
+				PurchaseDate: time.Now(),
+			}))
+
+			queryResponse := paperChaincode.Query("get", ISSUER_NAME, "0001")
+			paper := expectcc.PayloadIs(queryResponse, &CommercialPaper{}).(CommercialPaper)
+			Expect(paper.Owner).To(Equal(BUYER_NAME))
+			Expect(paper.State).To(Equal(CommercialPaperTrading))
+		})
+
+		It("Allow buyer to redeem commercial paper", func() {
+			//invoke chaincode method from authority actor
+			expectcc.ResponseOk(paperChaincode.Invoke(`redeem`, &RedeemCommercialPaper{
+				Issuer:         ISSUER_NAME,
+				PaperNumber:    "0001",
+				RedeemingOwner: BUYER_NAME,
+				RedeemDate:     time.Now(),
+			}))
+
+			queryResponse := paperChaincode.Query("get", ISSUER_NAME, "0001")
+			paper := expectcc.PayloadIs(queryResponse, &CommercialPaper{}).(CommercialPaper)
+			Expect(paper.Owner).To(Equal(ISSUER_NAME))
+			Expect(paper.State).To(Equal(CommercialPaperRedeemed))
+		})
+	})
+})
