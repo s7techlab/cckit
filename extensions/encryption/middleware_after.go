e2b8e4aff76118c14c34cdccd077e461c7691538
Ural Davletshin <u.davletshin@s7.ru>
2022-04-07 09:27:20 +0300

    Chaincode invocation response encryption

--- extensions/encryption/middleware_after.go
+++ extensions/encryption/middleware_after.go
@@ -4,7 +4,7 @@ import (
 	"github.com/s7techlab/cckit/router"
 )
 
-func EncryptInvokeResponse() router.MiddlewareFunc {
+func responseEncryptor(encryptionRequired bool) router.MiddlewareFunc {
 	return func(pre router.HandlerFunc, pos ...int) router.HandlerFunc {
 		return func(ctx router.Context) (interface{}, error) {
 			res, err := pre(ctx)
@@ -12,8 +12,22 @@ func EncryptInvokeResponse() router.MiddlewareFunc {
 			if err != nil || ctx.Handler().Type != router.MethodInvoke {
 				return res, err
 			}
+			var (
+				enc []byte
+			)
+			if enc, err = EncryptWithTransientKey(ctx, res); err == ErrKeyNotDefinedInTransientMap && !encryptionRequired {
+				return res, nil
+			}
 
-			return EncryptWithTransientKey(ctx, res)
+			return enc, err
 		}
 	}
+}
+
+func EncryptInvokeResponse() router.MiddlewareFunc {
+	return responseEncryptor(true)
+}
+
+func EncryptInvokeResponseIfKeyProvided() router.MiddlewareFunc {
+	return responseEncryptor(false)
 }
